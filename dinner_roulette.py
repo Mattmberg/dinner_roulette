2eb06ed447a1e5670e50e684e2d186b36a9de364
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2021-10-13 07:00:35 -0400

    Created initial Python file.
    Created the initial Python file. Imported Random library, set up initial list for dinner_sides, started setting up printing for the sides, and set up printing for food_choices.

---
+++ dinner_roulette.py
@@ -0,0 +1,10 @@
+import random
+
+food_choices = []
+
+dinner_sides = ['roasted potatoes', 'tater tots', 'green beans', 'cauliflower rice', 'corn', 'mixed vegetables', ]
+
+for i in range(1):
+  print(random.choice(food_choices))
+
+num_to_select = 2
