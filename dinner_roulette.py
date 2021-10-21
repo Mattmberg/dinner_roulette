e11b26e7d5a933f22b89ace58cccc00cd8438bea
Matthew Bergeron Jr <84132501+Mattmberg@users.noreply.github.com>
2021-10-20 22:00:20 -0400

    Updated food_choices
    Updated food_choices with dinners to be selected

--- dinner_roulette.py
+++ dinner_roulette.py
@@ -1,8 +1,8 @@
 import random
 
-food_choices = []
+food_choices = ['chicken parm','american chop suey','chicken fajitas','lemon pepper chicken','sheperds pie','beef stew','stuffed peppers']
 
-dinner_sides = ['roasted potatoes', 'tater tots', 'green beans', 'cauliflower rice', 'corn', 'mixed vegetables', ]
+dinner_sides = ['roasted potatoes', 'tater tots', 'green beans', 'cauliflower rice', 'corn', 'mixed vegetables']
 
 for i in range(1):
   print(random.choice(food_choices))
